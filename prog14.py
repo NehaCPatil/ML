882a0962c6725ab3290ddd615d11b1221373ea54
technick1113 <45517775+technick1113@users.noreply.github.com>
2019-03-04 09:45:43 +0530

    list all files in a directory

---
+++ prog14.py
@@ -0,0 +1,9 @@
+# Python program to list all files in a directory in Python.
+
+from os import listdir
+from os.path import isfile, join
+# isfile() : To check if the passed argument is valid file path
+# listdir(): To check if passed argument is valid directory path
+files_list = [f for f in listdir('/home/admin1') if isfile(join('/home/admin1', f))]
+# print file list
+print(files_list)
