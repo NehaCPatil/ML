20690f76b38572de0d6d11b6023785818d64a63d
technick1113 <45517775+technick1113@users.noreply.github.com>
2019-03-04 09:34:06 +0530

    display the first and last colors from list

---
+++ prog3.py
@@ -0,0 +1,9 @@
+# Python program to display the first and last colors from the following list.
+
+
+# Creating a list of string
+color_list = ["Red", "Green", "White", "Black"]
+# prints the given object to the standard output device (screen)
+print(color_list[0], color_list[-1])
+
+
