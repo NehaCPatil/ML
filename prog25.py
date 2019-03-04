af2f3f3eb5b0d3a27876d311bdbfa04a0c2a0c19
technick1113 <45517775+technick1113@users.noreply.github.com>
2019-03-04 09:54:37 +0530

    current value of the recursion limit

---
+++ prog25.py
@@ -0,0 +1,12 @@
+# Python program to get the current value of the recursion limit.
+
+# module provides access to some variables used or maintained by the interpreter
+# and to functions that interact strongly with the interpreter
+import sys
+#  Recursion implies it's the same thread over and over again that it's limiting.
+#  printing recursion limit
+print(sys.getrecursionlimit())
+# set recursion limit
+sys.setrecursionlimit(3000)
+# printing updated recursion limit
+print(sys.getrecursionlimit())
