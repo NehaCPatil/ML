282cee0fb17ad0141acdae14727c62ff7649d355
technick1113 <45517775+technick1113@users.noreply.github.com>
2019-03-04 09:51:28 +0530

    sort files by date

---
+++ prog21.py
@@ -0,0 +1,13 @@
+# Python program to sort files by date.
+
+# import glob to use glob() and related functions
+import glob
+# import os to use os related functions
+import os
+# glob.glob() to match specified pattern according to rules
+# asterisk is used to match zero or more characters
+files = glob.glob("*.py")
+# Sorting list of files in ascending
+files.sort(key=os.path.getmtime)
+# use os.path.join(), which joins paths using the correct path separator on the operating system
+print("\n".join(files))
