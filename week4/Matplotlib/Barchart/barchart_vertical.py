b2aa9da41cd297973a8af2f391f0d462d334b4f7
technick1113 <45517775+technick1113@users.noreply.github.com>
2019-03-25 14:47:39 +0530

    bar chart of the popularity of prog Lang

--- week4/Matplotlib/Barchart/barchart_vertical.py
+++ week4/Matplotlib/Barchart/barchart_vertical.py
@@ -1,5 +1,6 @@
 # 1. Write a Python programming to display a bar chart of the popularity of programming Languages.
 # Sample data:
+
 # Programming languages: Java, Python, PHP, JavaScript, C#, C++
 # Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
 
