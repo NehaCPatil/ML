54f76002f12e7a15b930cec699f2fab02f67af6d
NehaCPatil <45517775+NehaCPatil@users.noreply.github.com>
2019-04-26 15:02:19 +0530

    Delete prog11.py

--- prog11.py
+++
@@ -1,7 +0,0 @@
-# Python program to check whether a file exists.
-
-import os.path
-# Open function to open the file "abc.txt"
-open('abc.txt', 'w')
-# isfile() : To check if the passed argument is valid file path
-print(os.path.isfile('abc.txt'))
