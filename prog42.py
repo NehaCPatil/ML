9dda10ea9c903a9ce4ca74ec24212c1cce5a5f2f
NehaCPatil <45517775+NehaCPatil@users.noreply.github.com>
2019-04-26 15:13:32 +0530

    Delete prog42.py

--- prog42.py
+++
@@ -1,6 +0,0 @@
-# Python program to determine if the python shell is executing in 32bit or 64bit mode on operating system.
-
-# struct module can be used in handling binary data stored in files, database or from network connections
-import struct
-# Return the size of the struct (and hence of the string) corresponding to the given format
-print(struct.calcsize("P") * 8)
