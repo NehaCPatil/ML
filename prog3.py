c8c31f4a5762de2996da45ad3e149224f8706419
NehaCPatil <45517775+NehaCPatil@users.noreply.github.com>
2019-04-26 15:08:44 +0530

    Delete prog3.py

--- prog3.py
+++
@@ -1,9 +0,0 @@
-# Python program to display the first and last colors from the following list.
-
-
-# Creating a list of string
-color_list = ["Red", "Green", "White", "Black"]
-# prints the given object to the standard output device (screen)
-print(color_list[0], color_list[-1])
-
-
